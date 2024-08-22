e0fafca4143b234acf35f83a71d23fecf84f5b2a
Phạm Đình Đạt (VINBIGDATA-VNCDLL-CTÐTKSAI) <datpd1@vingroup.net>
2024-08-22 15:28:26 +0700

    upload load datasets

---
+++ download_datasets.py
@@ -0,0 +1,5 @@
+import opendatasets as op 
+
+url = "https://www.kaggle.com/datasets/lhanhsin/vizwiz"
+#14a9a942523de92e16dcc880dc405b2a
+op.download_kaggle_dataset(dataset_url=url, data_dir='./vizwiz')
